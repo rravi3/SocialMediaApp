39639dc2d791ddc9a80e38e1c9e47e94ed6ff0a0
rravi3 <ravirahul660@gmail.com>
2024-01-29 23:10:59 -0800

    Initial commit

---
+++ server/routes/posts.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+import { getPosts, getPost, getPostsBySearch, createPost, updatePost, commentPost, deletePost, likePost } from "../controllers/posts.js";
+import auth from '../middleware/auth.js';
+
+const router = express.Router();
+
+router.get('/', getPosts);
+router.get('/search', getPostsBySearch);
+router.get('/:id', getPost);
+router.post('/', auth, createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
+router.post('/:id/commentPost', auth, commentPost);
+
+export default router;
